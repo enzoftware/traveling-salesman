ba868e9a1d52ea4e44ac8064b1b9880aa282e214
Enzo Lizama <lizama.enzo@gmail.com>
2019-04-23 12:59:49 -0500

    get only necessary info

--- read_file.py
+++ read_file.py
@@ -8,12 +8,16 @@ class HeaderEnum(Enum):
     PROV = 2
     DIST = 3
     MNOMCP = 6
+    Y_X_COORD = 17
 
+
+included_cols = [0, 1, 2, 3, 6, 17]  # the cols of the wanted data
 
 with open('cpoblados.csv', 'r') as fin, open('outfile.csv', 'w') as fout:  # choose an input file to read and
     # a destination output file
     writer = csv.writer(fout, delimiter=',')  # divided by commas
     for row in csv.reader(fin, delimiter=','):
-        if row[HeaderEnum.DIST.value] == 'SAN JUAN DE LURIGANCHO':  # row in position 1 is DEPARTMENT /// row in position 2 is DISTRICT
-            writer.writerow(row)
+        if row[HeaderEnum.DIST.value] == 'VILLA EL SALVADOR':  # row in position 1
+            # is DEPARTMENT row in position 2 is DISTRICT
+            writer.writerow(list(row[i] for i in included_cols))
 print(writer)
