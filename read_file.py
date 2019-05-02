57ee0553919c6602f74f5b780ef48f03efa9fa16
Enzo Lizama <lizama.enzo@gmail.com>
2019-05-01 22:40:21 -0500

    change district to surco

--- read_file.py
+++ read_file.py
@@ -11,13 +11,14 @@ class HeaderEnum(Enum):
     Y_X_COORD = 17
 
 
-included_cols = [0, 1, 2, 3, 6, 17]  # the cols of the wanted data
+included_cols = [0, 3, 6, 15, 16]  # the cols of the wanted data
 
 with open('cpoblados.csv', 'r') as fin, open('outfile.csv', 'w') as fout:  # choose an input file to read and
     # a destination output file
     writer = csv.writer(fout, delimiter=',')  # divided by commas
     for row in csv.reader(fin, delimiter=','):
-        if row[HeaderEnum.DIST.value] == 'VILLA EL SALVADOR':  # row in position 1
+        if row[HeaderEnum.DIST.value] == 'SURCO':  # row in position 1
             # is DEPARTMENT row in position 2 is DISTRICT
             writer.writerow(list(row[i] for i in included_cols))
 print(writer)
+
