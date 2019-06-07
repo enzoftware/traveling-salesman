1371c537fe875c3906d703b870267e73ace51bca
Miguel Delgado <vcaperu@outlook.com>
2019-06-07 18:47:13 -0500

    seleccionar capitales

---
+++ read_file.py
@@ -0,0 +1,33 @@
+import csv
+from enum import Enum
+
+class HeaderEnum(Enum):
+
+    UBIGEO = 0
+    DEP = 1
+    PROV = 2
+    DIST = 3
+    CODCP = 4
+    MNOMCP = 6
+    CAPITAL = 7
+    XGD = 15
+    YGD = 16
+
+included_cols = [0, 1, 2, 3, 4, 6, 15, 16]  # the cols of the wanted data
+
+
+with open('cpoblados.csv', 'r') as fin, open('outfile.csv', 'w') as fout:  # choose an input file to read and
+
+    # a destination output file
+
+    writer = csv.writer(fout, delimiter=',')  # divided by commas
+
+    for row in csv.reader(fin, delimiter=','):
+
+        if row[HeaderEnum.CAPITAL.value] == 1 or row[HeaderEnum.CAPITAL.value] == '1' :  # row in position 1
+
+            # is DEPARTMENT row in position 2 is DISTRICT
+
+            writer.writerow(list(row[i] for i in included_cols))
+
+print(writer)
