83b9ec8070d0145744f426e134873bc0b96d57f0
Miguel Delgado <vcaperu@outlook.com>
2019-06-10 15:11:20 -0500

    capitales nivel 1-12 capitales nivel 2-82 capitales nivel 3-770

--- read_file.py
+++ read_file.py
@@ -16,18 +16,19 @@ class HeaderEnum(Enum):
 included_cols = [0, 1, 2, 3, 4, 6, 15, 16]  # the cols of the wanted data
 
 
-with open('cpoblados.csv', 'r') as fin, open('outfile.csv', 'w') as fout:  # choose an input file to read and
+with open('cpoblados.csv', 'r') as fin, open('outfile3.csv', 'w') as fout:  # choose an input file to read and
 
     # a destination output file
 
     writer = csv.writer(fout, delimiter=',')  # divided by commas
-
+    count = 0
     for row in csv.reader(fin, delimiter=','):
 
-        if row[HeaderEnum.CAPITAL.value] == 1 or row[HeaderEnum.CAPITAL.value] == '1' :  # row in position 1
+        if row[HeaderEnum.CAPITAL.value] == 3 or row[HeaderEnum.CAPITAL.value] == '3' :  # row in position 1
 
             # is DEPARTMENT row in position 2 is DISTRICT
-
+            count = count + 1
             writer.writerow(list(row[i] for i in included_cols))
+    print(count)
 
 print(writer)
